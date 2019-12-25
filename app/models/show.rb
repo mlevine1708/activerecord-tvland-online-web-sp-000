22d9f85d414310a17c51beeceb1f67b5187621fd
Micah Levine <micah.levine@outlook.com>
2019-12-25 03:49:04 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,6 +2,7 @@ class Show < ActiveRecord::Base
 
   has_many :characters
   belongs_to :network
+  has_many :actors, through: :characters
 
   def actors_list
     self.actors.map{|actor| actor.full_name}
