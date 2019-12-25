85b6a1a2a654a24e40eaabcc1c50e70f6eb5c31c
Micah Levine <micah.levine@outlook.com>
2019-12-25 03:49:18 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,9 +2,9 @@ class Show < ActiveRecord::Base
 
   has_many :characters
   belongs_to :network
+  has_many :actors, through: :characters
 
   def actors_list
-    self.actor.map
+    self.actors.map{|actor| actor.full_name}
   end
-end 
 end
